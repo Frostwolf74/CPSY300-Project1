1df735f6c79d49ed8f1cdc72a58c5f30bd42c71a
Frostwolf74 <152447084+Frostwolf74@users.noreply.github.com>
2026-02-11 16:13:46 -0700

    initial commit

---
+++ data_analysis.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def process_data():
+    df = pd.read_csv("All_Diets.csv")
+    df.fillna(df.astype().mean(), inplace=True)
+
+    avg_macros = df.groupby('Diet_type')[['Protein(g)', 'Carbs(g)', 'Fat(g)']].mean()
+    top_protein = df.sort_values('Protein(g)', ascending=False).groupby('Diet_type').head(5)
+
+    df['Protein_to_Carbs_ratio'] = df['Protein(g)'] / df['Carbs(g)']
+    df['Carbs_to_Fat_ratio'] = df['Carbs(g)'] / df['Fat(g)']
+
+
+    ## visualize data for macronutrients
+    # bar graph to show average protein for each diet type
+    sns.barplot(x=avg_macros.index, y=avg_macros['Protein(g)'])
+    plt.title('Average Protein by Diet Type')
+    plt.ylabel('Average Protein (g)')
+    plt.show()
+
+    # bar graph to show average carbs for each diet type
+    sns.barplot(x=avg_macros.index, y=avg_macros['Carbs(g)'])
+    plt.title('Average Protein by Diet Type')
+    plt.ylabel('Average Carbs (g)')
+    plt.show()
+
+    sns.barplot(x=avg_macros.index, y=avg_macros['Fat(g)'])
+    plt.title('Average Fat by Diet Type')
+    plt.ylabel('Average Fat (g)')
+    plt.show()
+
+
+    sns.heatmap(data=avg_macros['Protein(g)'], cmap='YlGnBu')
+
+if __name__ == '__main__':
+    process_data()
